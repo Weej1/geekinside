3e3a49f3b940532b5f6decc59bde5935405e5145
dygao66 <dygao66@gmail.com>
2012-06-30 02:08:53 +0000

    修复了管理员登陆时用户名密码错误后报错的BUG，更新了模型文件（GeekInsideEF.edmx）

--- GeekInsideKMS/DAL/DALAdminAcount.cs
+++ GeekInsideKMS/DAL/DALAdminAcount.cs
@@ -9,6 +9,19 @@ namespace DAL
 {
     public class DALAdminAcount:IDALAdminAcount
     {
+        private UserAdminModel ConvertFromDB(UserAdmin dbAdmin)
+        {
+            if (dbAdmin == null) return null;
+            return new UserAdminModel
+            {
+                Id = dbAdmin.Id,
+                EmployeeNumber = dbAdmin.EmployeeNumber,
+                Username = dbAdmin.Username,
+                Password = dbAdmin.Password,
+                LastLoginTime = dbAdmin.LastLoginTime
+            };
+        }
+
         public UserAdminModel getUserByUsername(string username)
         {
             using (var gikms = new geekinsidekmsEntities())
@@ -16,11 +29,10 @@ namespace DAL
                 var userAdmin = from u in gikms.UserAdmins
                                 where u.Username.Equals(username)
                                 select u;
-
-                UserAdminModel result = new UserAdminModel();
-                result.Username = userAdmin.First().Username;
-                result.Password = userAdmin.First().Password;
-                return result;
+                UserAdmin dbAdmin = (from u in gikms.UserAdmins
+                                           where u.Username.Equals(username)
+                                           select u).FirstOrDefault();
+                return ConvertFromDB(dbAdmin);
             }
         }
     }
--- GeekInsideKMS/Model/Models/UserAdminModel.cs
+++ GeekInsideKMS/Model/Models/UserAdminModel.cs
@@ -7,24 +7,10 @@ namespace Model.Models
 {
     public class UserAdminModel
     {
-        public UserAdminModel()
-        {
-
-        }
-
-        public UserAdminModel(int Id, int EmployeeNumber, string Username, string Password, DateTime LastLoginTime)
-        {
-            this.Id = Id;
-            this.EmployeeNumber = EmployeeNumber;
-            this.Username = Username;
-            this.Password = Password;
-            this.LastLoginTime = LastLoginTime;
-        }
-
         public int Id { get; set; }
         public int EmployeeNumber { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
-        public DateTime LastLoginTime { get; set; }
+        public DateTime? LastLoginTime { get; set; }
     }
 }
