0a1dfc7ecbd5a54aedf021bd84dcfc1867b81382
dygao66 <dygao66@gmail.com>
2012-06-30 02:47:23 +0000

    添加DALFactory和Utils两个类库： 1. DALFactory用于构造DAL对象，BLL中可以通过DALFactory得到具体的IDAL接口实现对象 2. Utils类库中有个Helper类，可以用来添加一些常用的函数，我目前只加了使用MD5加密的方法，可以在需要加密的地方直接使用 修复： 1. 修复了DALAdminAccount的拼写错误= =

---
+++ GeekInsideKMS/DALFactory/DataAccess.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using System.Configuration;
+using IDAL;
+
+namespace DALFactory
+{
+    public class DataAccess
+    {
+        private static readonly string path = ConfigurationManager.AppSettings["WebDAL"];
+
+        private DataAccess() {}
+
+        public static IDALAdminAcount CreateAdminDAL()
+        {
+            string className = path + ".DALAdminAccount";
+            return (IDALAdminAcount)Assembly.Load(path).CreateInstance(className);
+        }
+
+        public static IDALSiteConfig CreateSiteConfiguraionDAL()
+        {
+            string className = path + ".DALSiteConfig";
+            return (IDALSiteConfig)Assembly.Load(path).CreateInstance(className);
+        }
+    }
+}
---
+++ GeekInsideKMS/Utils/Helper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Security.Cryptography;
+
+namespace Utils
+{
+    public class Helper
+    {
+        private Helper() { }
+
+        // 加密text，返回32的密文
+        public static string EncryptByMD5(string text)
+        {
+            MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
+            string strings = BitConverter.ToString(md5.ComputeHash(Encoding.UTF8.GetBytes(text))).Replace("-", string.Empty);
+            return strings;
+        }
+    }
+}
