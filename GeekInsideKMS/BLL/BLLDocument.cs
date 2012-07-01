5f0f3c8df1d8f9f601133366e7a05928fedd6319
zs910504 <zs910504@gmail.com>
2012-07-01 06:59:02 +0000

    文档上载

---
+++ GeekInsideKMS/BLL/BLLDocument.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using Model.Models;
+
+namespace BLL
+{
+    public class BLLDocument
+    {
+        public void DeleteTempFile(string id, string fileName)
+        {
+            string extension = fileName.Substring(fileName.LastIndexOf('.'));
+            string filePath = Path.Combine("D://geekinsidekms/temp/", id + extension);
+            if (File.Exists(filePath))
+            {
+                File.Delete(filePath);
+            }
+        }
+
+        public bool AddDocument(DocumentModel document)
+        {
+            return true;
+        }
+    }
+}
---
+++ GeekInsideKMS/Index/UploadHandler.ashx.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IO;
+using System.Collections.Specialized;
+
+namespace Index
+{
+    /// <summary>
+    /// UploadHandler 的摘要说明
+    /// </summary>
+    public class UploadHandler : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            IServiceProvider provider = (IServiceProvider)context;
+            HttpWorkerRequest wr = (HttpWorkerRequest)provider.GetService(typeof(HttpWorkerRequest));
+
+            int count = 8192;
+            byte[] buffer;
+
+            if (!wr.IsEntireEntityBodyIsPreloaded())
+            {
+                string chunk = context.Request["chunk"];
+                string chunks = context.Request["chunks"];
+                string fileName = context.Request["name"];
+                string id = context.Request["id"];
+
+                string extension = fileName.Substring(fileName.LastIndexOf('.'));
+                buffer = new Byte[count];
+                string filePath = Path.Combine("D://geekinsidekms/temp/", id+extension);
+                using (var fs = new FileStream(filePath, chunk.Equals("0") ? FileMode.Create : FileMode.Append))
+                {
+                    if (context.Request.Files.Count > 0)
+                    {
+                        HttpPostedFile httpFile = context.Request.Files[0];
+                        while (true)
+                        {
+                            count = httpFile.InputStream.Read(buffer, 0, count);
+                            if (count == 0)
+                            {
+                                break;
+                            }
+                            saveFile(fs, buffer, count);
+                        }
+                    }
+                    fs.Close();
+                    fs.Dispose();
+                }
+
+                if (Convert.ToInt32(chunk) == (Convert.ToInt32(chunks) - 1))
+                {
+                    context.Response.Write("success");
+                }
+            }
+
+        }
+
+        public void saveFile(FileStream fs, Byte[] buffer, int count)
+        {
+            fs.Write(buffer, 0, count);
+        }
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
