0b52c47052ed6872260b8cc255aa0a6b3bee5a96
zhaoyulee.works <zhaoyulee.works@c78d5438-20b2-4f74-30ea-a8d87c6ae023>
2012-07-01 17:25:08 +0000

    Usercontroller中empno获取改为session取值；删除我的文档功能完成；收藏完成；编辑除保存外完成

--- GeekInsideKMS/DAL/DALFavorite.cs
+++ GeekInsideKMS/DAL/DALFavorite.cs
@@ -34,5 +34,29 @@ namespace DAL
                 return favDocModelList;
             }
         }
+
+        public Boolean deleteFavById(int employeeNumber, int documentId)
+        {
+            geekinsidekmsEntities context = new geekinsidekmsEntities();
+
+            DAL.Favorite dbFav = (from fav in context.Favorites
+                                  where fav.DocumentId.Equals(documentId) && fav.EmployeeNumber.Equals(employeeNumber)
+                                  select fav).FirstOrDefault();
+            context.DeleteObject(dbFav);
+            context.SaveChanges();
+            return true;
+        }
+
+        public Boolean addFav(int employeeNumber, int documentId)
+        {
+            geekinsidekmsEntities context = new geekinsidekmsEntities();
+
+            DAL.Favorite fav = new DAL.Favorite();
+            fav.EmployeeNumber = employeeNumber;
+            fav.DocumentId = documentId;
+            context.AddToFavorites(fav);
+            context.SaveChanges();
+            return true;
+        }
     }
 }
