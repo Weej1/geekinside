304ebe47b6394ff4c14e1a81ab68b327cdaf3561
kylewang1005 <kylewang1005@gmail.com>
2012-07-01 14:07:32 +0000

    增加了管理员添加用户功能，对前台和后台的登录验证添加了MD5加密 新增了UserEmployeeDetailModel，更正了UserEmployeeModel, 添加了相应层的对于用户的增删改查的操作方法（BLL，DAL）

--- GeekInsideKMS/Admin/Controllers/EmployeeController.cs
+++ GeekInsideKMS/Admin/Controllers/EmployeeController.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Security;
+using Model.Models;
+using BLL;
+using System.Text.RegularExpressions;
 
 namespace Admin.Controllers
 {
@@ -13,6 +16,62 @@ namespace Admin.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        [Authorize]
+        public ActionResult CreateUser()
+        {
+            ViewData["employeeNumber"] = new BLLUserAccount().GetMaxEmployeeNumber() + 1;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult doCreateUser() 
+        {
+            string REGEXP_IS_VALID_EMAIL = @"^\w+((-\w+)(\.\w+))*\@\w+((\.-)\w+)*\.\w+$";  //电子邮件校验常量
+            string REGEXP_IS_VALID_PHONE = @"/^\d{7,8}$/"; //手机号校验常量
+
+            BLLUserAccount bllUserAccount = new BLLUserAccount();
+            UserEmployeeModel userEmployeeModel = new UserEmployeeModel();
+            UserEmployeeDetailModel userEmployeeDetailModel = new UserEmployeeDetailModel();
+
+            userEmployeeModel.EmployeeNumber = bllUserAccount.GetMaxEmployeeNumber() + 1;            
+            userEmployeeModel.Password = "123456";
+            userEmployeeModel.DepartmentId = Convert.ToInt16(Request.Form["departmentId"]);
+            userEmployeeModel.IsManager = (Convert.ToInt16(Request.Form["isManager"])==0 ? false : true);
+            userEmployeeModel.IsChecker = (Convert.ToInt16(Request.Form["isChecker"])==0 ? false : true);
+            userEmployeeModel.IsAvailable = (Convert.ToInt16(Request.Form["isAvailable"])==0 ? false : true);
+
+            userEmployeeDetailModel.EmployeeNumber = Convert.ToInt16(Request.Form["employeeNumber"]);
+            userEmployeeDetailModel.Name = Request.Form["name"];
+            userEmployeeDetailModel.Email = Request.Form["email"];
+            userEmployeeDetailModel.Phone = Request.Form["phone"];
+
+            if (userEmployeeDetailModel.Email == null || !Regex.IsMatch(userEmployeeDetailModel.Email, REGEXP_IS_VALID_EMAIL)) 
+            {
+                TempData["employeeNumberErrorMsg"] = "请输入正确的邮箱地址！";
+                return RedirectToAction("CreateUser", "Employee");
+            }
+
+            bool phoneResult = Regex.IsMatch(userEmployeeDetailModel.Phone, REGEXP_IS_VALID_EMAIL);
+            if (userEmployeeDetailModel.Phone == null || !phoneResult)
+            {
+                TempData["phoneErrorMsg"] = "请输入正确的手机号！";
+                return RedirectToAction("CreateUser", "Employee");
+            }
+
+            Boolean result = bllUserAccount.CreateUserAccount(userEmployeeModel, userEmployeeDetailModel);
+            
+            if (result == true)
+            {
+                ViewData["successMsg"] = "添加成功";
+                return RedirectToAction("CreateUser", "Employee");
+            }
+            else 
+            {
+                ViewData["errorMsg"] = "添加失败";
+                return RedirectToAction("CreateUser", "Employee");
+            }
         }
 
     }
---
+++ GeekInsideKMS/Model/Models/UserEmployeeDetailModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Model.Models
+{
+    public class UserEmployeeDetailModel
+    {
+        public int Id { get; set; }
+        public int EmployeeNumber { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+    }
+}
