a013387611a94bd0a111afc8999df2fe8103ae4c
kylewang1005 <kylewang1005@gmail.com>
2012-07-01 14:11:55 +0000

    解决冲突BLLUserAccount

--- GeekInsideKMS/BLL/BLLUserAccount.cs
+++ GeekInsideKMS/BLL/BLLUserAccount.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using Model.Models;
 using IDAL;
+using Utils;
 
 namespace BLL
 {
@@ -14,7 +15,7 @@ namespace BLL
         public Boolean CheckUserLogin(UserEmployeeModel userEmployeeModel)
         {
             UserEmployeeModel user = userDAL.getUserByEmployeeNumber(userEmployeeModel.EmployeeNumber);
-            if (user != null && user.Password == userEmployeeModel.Password)
+            if (user != null && this.CheckPassword(user, userEmployeeModel.Password))
             {
                 return true;
             }
@@ -22,6 +23,46 @@ namespace BLL
             {
                 return false;
             }
+        }
+
+        public Boolean CheckPassword(UserEmployeeModel userEmployeeModel, string password)
+        {
+            string encryptPassword = Helper.EncryptByMD5(password);
+            return userEmployeeModel.Password == encryptPassword;
+        }
+
+        public Boolean CreateUserAccount(UserEmployeeModel userEmployeeModel, UserEmployeeDetailModel userEmployeeDetailModel)
+        {
+            userEmployeeModel.Password = Helper.EncryptByMD5(userEmployeeModel.Password);
+            userDAL.CreateUserAccount(userEmployeeModel, userEmployeeDetailModel);
+            return true;
+        }
+
+        public Boolean DeleteUserAccount(UserEmployeeModel userEmployeeModel, UserEmployeeDetailModel userEmployeeDetailModel)
+        {
+            userDAL.DeleteUserAccount(userEmployeeModel, userEmployeeDetailModel);
+            return true;
+        }
+
+        public Boolean UpdateUserAccount(UserEmployeeModel userEmployeeModel, UserEmployeeDetailModel userEmployeeDetailModel)
+        {
+            userDAL.UpdateUserAccount(userEmployeeModel, userEmployeeDetailModel);
+            return true;
+        }
+
+        public UserEmployeeDetailModel GetUserDetailByEmpNumber(int empNumber)
+        {
+            return userDAL.GetEmployeeDetailByEmployeeNumber(empNumber);
+        }
+
+        public UserEmployeeModel GetUserByEmpNumber(int empNumber)
+        {
+            return userDAL.getUserByEmployeeNumber(empNumber);
+        }
+
+        public int GetMaxEmployeeNumber()
+        {
+            return userDAL.GetMaxEmployeeNumber();
         }
 
         public List<DocumentModel> getAllMyDocument()
