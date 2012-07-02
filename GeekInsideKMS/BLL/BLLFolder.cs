c3e07344b7d3b3d0429b9caf09b46d055e1d8337
zs910504@gmail.com <zs910504@gmail.com@c78d5438-20b2-4f74-30ea-a8d87c6ae023>
2012-07-02 08:59:44 +0000

    folder 业务cheng

--- GeekInsideKMS/BLL/BLLFolder.cs
+++ GeekInsideKMS/BLL/BLLFolder.cs
@@ -1,14 +1,22 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Model.Models;
+using DALFactory;
+using IDAL;
 
 namespace BLL
 {
     public class BLLFolder
     {
         IDAL.IDALFolder folderDAL = DALFactory.DataAccess.CreateFolderDAL();
+
+        public IList<FolderModel> GetAllFolders()
+        {
+            IDALFolder folderDAL= DALFactory.DataAccess.CreateFolderDAL();
+            return folderDAL.GetFolderStructure();
+        }
 
         public FolderModel GetFolderById(int folder_id)
         {
@@ -21,3 +29,4 @@ namespace BLL
         }
     }
 }
+
